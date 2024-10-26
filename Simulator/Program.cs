855922b2bde98a95ec5db5de71f3c85a995bbd5b
WojtowiczZuzanna <zuzia.wojtowicz2004@gmail.com>
2024-10-26 14:31:24 +0200

    Add project files.

---
+++ Simulator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Simulator;
+//file-scoped namespace == with ";"
+//blockck-scoped namespace == with "{}"
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting Simulator!\n");
+    }
+}
