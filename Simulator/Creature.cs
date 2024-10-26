1dc4069b29b2f8ef04ba85017025c20f9b5f29fd
WojtowiczZuzanna <zuzia.wojtowicz2004@gmail.com>
2024-10-26 18:44:46 +0200

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+public class Creature
+{
+    public string Name { get; set; } = "Unknown";
+    public int Level { get; set; } = 1;
+
+    public Creature() { }
+
+    public string Info => $"{Name} {Level}";
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    }
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -6,5 +6,14 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
+
+        Creature c1 = new();
+        c1.SayHi();
+
+        Creature c2 = new Creature();
+        c2.Name = "Dracula";
+        c2.Level = 300;
+        c2.SayHi();
+
     }
 }
